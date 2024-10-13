e7a0c759c5d20390826ba4128bf3ba7f3abb7e0c
tayade8767 <111789993+tayade8767@users.noreply.github.com>
2024-10-13 19:42:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: *,
+    origin: '*',
     credentials:true
 }
 
